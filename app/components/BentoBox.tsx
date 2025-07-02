3808d70ff632803cb1a57644461eefb154f4f56a
HugoBarnes <hugo.s.barnes@gmail.com>
2025-07-02 02:26:22 -0400

    about to publish

--- app/components/BentoBox.tsx
+++ app/components/BentoBox.tsx
@@ -6,12 +6,12 @@ const projectData = [
     { name: 'Scrabble Solver',link: "/Projects/ScrabbleSolver", description: "Takes letters and the board state as input. Outputs the optimal move."},
     { name: '3D Printed Drawing Machine', link:"/Projects/DrawingMachine", description:"An arduino (cnc extension) powered drawing machine. Inspired by the movie Hugo."},
     { name: 'Calvin and Hobbes Reader', link:"/Projects/CalvinAndHobbes", description:"Downloaded from the Internet Archive. The complete Calvin and Hobbes Collection."},
-    { name: 'Sudoku Solver', link:"/", description: "Try a solvable Sudoku! Or have it solved for you."},
-    { name: 'Crossword Team Play', link:"/", description:"Get a previous New York Times Crossword and Solve it with a friend."},
-    { name: 'Reinforcement Learning Tic-Tac-Toe', link:"/",description:"Example from Sutton's Reinforcement Learning Textbook."},
-    { name: 'Neighborhood Work App', link:"/",description: "A web app that allows homeowners to connect with contractors."},
+    { name: 'Sudoku Solver', link:"/Projects/Sudoku", description: "Try a solvable Sudoku! Or have it solved for you."},
+    //{ name: 'Crossword Team Play', link:"/", description:"Get a previous New York Times Crossword and Solve it with a friend."},
+    //{ name: 'Reinforcement Learning Tic-Tac-Toe', link:"/",description:"Example from Sutton's Reinforcement Learning Textbook."},
+    //{ name: 'Neighborhood Work App', link:"/",description: "A web app that allows homeowners to connect with contractors."},
     { name: 'Linear Algebra Calculator', link:"/Projects/LinAlgebra",description:"A web app for Matrix computations."},
-    {name: 'RailTracker', link:"/Projects/AmtrakTracker", description:"A lightweight web app for tracking trains."}
+    //{name: 'RailTracker', link:"/Projects/AmtrakTracker", description:"A lightweight web app for tracking trains."}
 ];
 
 const BentoBox = () =>{
